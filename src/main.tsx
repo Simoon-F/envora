efd8d1d3356b76b99abddef8c4428491d233bf0b
Simon <simoonf@vip.qq.com>
2026-06-13 23:51:27 +0800

    feat: add main application structure with routing and styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { SWRConfig } from 'swr';
-import App from './app-root';
+import App from './app';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
